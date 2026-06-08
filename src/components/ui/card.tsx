b14f6c2b27c6c8512f8d438334ae67ccb16b4497
Fzi1 <muhammadarkanfauzi9@gmail.com>
2026-06-08 09:00:04 +0700

    fixMerge: memperbaiki conflict merge branch faldan_dev dan dev

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -13,11 +13,7 @@ function Card({
       data-slot="card"
       data-size={size}
       className={cn(
-<<<<<<< HEAD
-        "group/card flex flex-col gap-(--card-spacing) overflow-hidden rounded-4xl bg-card py-(--card-spacing) text-sm text-card-foreground shadow-md ring-1 ring-foreground/5 [--card-spacing:--spacing(6)] has-[>img:first-child]:pt-0 data-[size=sm]:[--card-spacing:--spacing(4)] dark:ring-foreground/10 *:[img:first-child]:rounded-t-4xl *:[img:last-child]:rounded-b-4xl",
-=======
         "group/card flex flex-col gap-(--card-spacing) overflow-hidden rounded-lg bg-card py-(--card-spacing) text-xs/relaxed text-card-foreground ring-1 ring-foreground/10 [--card-spacing:--spacing(4)] has-[>img:first-child]:pt-0 data-[size=sm]:[--card-spacing:--spacing(3)] *:[img:first-child]:rounded-t-lg *:[img:last-child]:rounded-b-lg",
->>>>>>> ee1f51ead7f09bd838620ec8220ee39cb6f956be
         className
       )}
       {...props}
@@ -30,11 +26,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-header"
       className={cn(
-<<<<<<< HEAD
-        "group/card-header @container/card-header grid auto-rows-min items-start gap-1.5 rounded-t-4xl px-(--card-spacing) has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto] [.border-b]:pb-(--card-spacing)",
-=======
         "group/card-header @container/card-header grid auto-rows-min items-start gap-1 rounded-t-lg px-(--card-spacing) has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto] [.border-b]:pb-(--card-spacing)",
->>>>>>> ee1f51ead7f09bd838620ec8220ee39cb6f956be
         className
       )}
       {...props}
@@ -46,11 +38,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-<<<<<<< HEAD
-      className={cn("font-heading text-base font-medium", className)}
-=======
       className={cn("font-heading text-sm font-medium", className)}
->>>>>>> ee1f51ead7f09bd838620ec8220ee39cb6f956be
       {...props}
     />
   )
@@ -60,11 +48,7 @@ function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-description"
-<<<<<<< HEAD
-      className={cn("text-sm text-muted-foreground", className)}
-=======
       className={cn("text-xs/relaxed text-muted-foreground", className)}
->>>>>>> ee1f51ead7f09bd838620ec8220ee39cb6f956be
       {...props}
     />
   )
@@ -98,11 +82,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-footer"
       className={cn(
-<<<<<<< HEAD
-        "flex items-center rounded-b-4xl px-(--card-spacing) [.border-t]:pt-(--card-spacing)",
-=======
         "flex items-center rounded-b-lg px-(--card-spacing) [.border-t]:pt-(--card-spacing)",
->>>>>>> ee1f51ead7f09bd838620ec8220ee39cb6f956be
         className
       )}
       {...props}
